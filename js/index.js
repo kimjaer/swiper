681a23d986a2738a069b64ab49d9f12f72a912b2
kimjaer <wofus0422@gmail.com>
2023-11-06 12:09:23 +0900

    rel,abs 줬을 때

--- js/index.js
+++ js/index.js
@@ -1,19 +1,15 @@
 $(document).ready(function () {
-  //setInterval , animate , css , appendTo(객체 위치 바꾸기/복사기능)
-  // setInterval(function(){}, 3000) 
-  // $(태그).animate({}, speed, function(){})
-  // $(태그).css(스타일, 스타일 값)
-  // $(옮길태그).appendTo($(부모가 될 태그))
 
   let count = 0;
 
-  setInterval(fadeani, 3000)
 
-  function fadeani() { // 선언적 함수
-    count++;
+  setInterval(function () {
+    count++; 
     count = count % 3; //3이 오는 주기마다 0으로 바꿈
     $(".swiper-slide").eq(count).addClass("on").siblings().removeClass("on")
-  
-  }
+
+
+  }, 3000)
 })
+
 
